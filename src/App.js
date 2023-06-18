0026aac4f28d57bc5f08c5e1b47e6a2f8aee4aec
the-null-character <akshay.2020a@vitstudent.ac.in>
2023-06-18 21:42:09 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import ClientTable from './ClientTable';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://dummyjson.com/users');
+      const data = await response.json();
+      setUsers(data.users);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
   return (
     <div>
-      <ClientTable />
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>MaidenName</th>
+            <th>Age</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.maidenName}</td>
+              <td>{user.age}</td>
+              <td>{user.email}</td>
+              <td>{user.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
